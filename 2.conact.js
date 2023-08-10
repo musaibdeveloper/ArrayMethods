223919d110b68f8f803bf03425c29f665dc5c8c4
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-10 17:33:32 +0000

    Update of Concat

--- 2.conact.js
+++ 2.conact.js
@@ -1,4 +1,10 @@
-let arr = ["one", "two", ]
+// Concat methods Returns array by merging given value and/or arrays. 
+// Simple it adds to arrays or merge. 
+
+// The array which should be added must be under ()
+
+
+et arr = ["one", "two", ]
 let arr2 = ["three","four"]
 
 
