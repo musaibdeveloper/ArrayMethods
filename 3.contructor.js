ef846eb6036885c7a56454d4ffe31d0147f7ce5f
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-10 17:42:51 +0000

    Constructor

---
+++ 3.contructor.js
@@ -0,0 +1,12 @@
+// Array Constructor()
+
+// Array constructor return the Data type of array in a function way. 
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "musaib"];
+
+let store = arr.constructor;
+
+console.log(store);
+
+// Returns the constructor function for the array.
+ 
