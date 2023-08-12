460589d1e21bf398eea962b8f96222dab5b9ae81
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 16:05:56 +0000

    push is updated

---
+++ 18.push.js
@@ -0,0 +1,12 @@
+// /Array push method 
+
+// The push() method adds zero or more elements to the end of the array.
+
+
+let arr = ["musaib", "syed", "Abdul"];
+
+let push = arr.push("saboor","shoail");
+
+console.log(push)
+
+// ["musaib", "syed". "Abdul" , "saboor", "shoail"]
