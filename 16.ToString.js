10a203b1b86a754bfc6fee0c1a5599a43768161d
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 15:39:28 +0000

    Tostring is updated

---
+++ 16.ToString.js
@@ -0,0 +1,20 @@
+// Array method to String. 
+
+// This method returns a string
+// representing the array and its elements.
+
+// The toString() method does not change the original array.
+
+let arr = ["musaib", "syed", "abdul", "saboor"]
+
+let result = (arr.toString());
+// let result = typeof((arr.toString()));
+
+console.log(result);
+
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let numresult = array1.toString();
+
+console.log(numresult);
+
