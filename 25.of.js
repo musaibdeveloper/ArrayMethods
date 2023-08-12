b3e9775d297b6d3285f4424b0e17482dd899a1cc
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 19:16:26 +0000

    Of os updated

---
+++ 25.of.js
@@ -0,0 +1,20 @@
+// Array method of ()
+
+// The of() method creates a new Array instance from the given arguments.
+
+
+const arr = [12,5,9,6,4,59,]
+
+console.log(arr);
+
+// Its a simple array :) 
+
+// Now Array of ()
+
+const arr1 = Array.of(1,2,3,4,56,7,8,9)
+
+console.log(arr1);
+
+
+let data = Array.of("Hello", "Java", "C", "C++");
+console.log(data);
