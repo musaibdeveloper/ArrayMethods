ec90808487e6273e869dc257cb968a6278fb6fd9
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 17:07:57 +0000

    Unshift is added

---
+++ 20.Unshift.js
@@ -0,0 +1,16 @@
+// Array method
+
+/*
+unshift() method adds one or more elements to
+the beginning of an array and returns the new
+length of the array.
+
+It addes the value in the 0 position of the array. 
+
+*/
+
+let arr = [1,23,66,56956,54545]
+
+let add = arr.unshift(5454);
+
+console.log(arr);
