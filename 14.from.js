d7734dfca88f5bb3310b64c0418596ba14365783
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 15:11:56 +0000

    From is updated

---
+++ 14.from.js
@@ -0,0 +1,17 @@
+// From method in arrays
+// 1. from is used to create a array from any objects. 
+
+// The syntax of the method is from () :
+
+
+
+let names = "Musaib"
+
+console.log(Array.from(names));
+ 
+
+// Number to array. 
+
+let num = "12345";
+console.log(Array.from(num));
+console.log(num);
