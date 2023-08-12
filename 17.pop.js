963ad407ef99779c5683c031d362d12da3ce4c44
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 15:56:32 +0000

    Pop os updated

---
+++ 17.pop.js
@@ -0,0 +1,12 @@
+// /Array method pop. 
+
+// : This method removes the last element
+// from an array and returns that element.
+
+// It removes the last element only
+
+let arr = ["Apple", "Banana", "Mango", "cherry"]
+
+let result = arr.pop();
+
+console.log(arr);
