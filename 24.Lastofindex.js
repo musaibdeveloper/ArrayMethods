e61dbd8cb7ffe11186f873e6c9d8695722517337
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 19:07:35 +0000

    Lastof index is Updated

---
+++ 24.Lastofindex.js
@@ -0,0 +1,14 @@
+// Array lastIndexof()
+//The lastIndexOf() method returns the index of
+// the last occurrence of a specified element in the array.
+// -1 if the element is not found in the array.
+
+
+let num = [1,2,6,7,8,9,3]
+
+console.log(num.lastIndexOf(1));
+
+// The above last of index takes the input as element which is present in the array, 
+// gives the index value, if the element doesn't exist in the array then it's -1 by default.
+
+
