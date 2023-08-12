9a0e5d2792042a7d101ad0806b40a7021a680628
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 17:02:09 +0000

    Push method is updated

---
+++ 19.shift.js
@@ -0,0 +1,12 @@
+// Array method Shift.
+// shift() method removes the first element from
+// an array and returns that removed element.
+// This method changes the length of the array.
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+let remove = fruits.shift();
+
+console.log(remove);
+
+// Output Apple. 
