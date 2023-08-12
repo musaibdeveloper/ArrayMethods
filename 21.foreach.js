48086d293568cfd81074fa7e09a9e249a2887a4b
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-12 17:35:24 +0000

    Foreach is updated

---
+++ 21.foreach.js
@@ -0,0 +1,16 @@
+// Array for each method.
+
+
+// The forEach() method calls a function for each element in an array.
+// The forEach() method is not executed for empty elements.
+
+// For each methos execute the function for every element in the array. 
+
+let number = [1,2,3,4,56,78];
+
+number.forEach((x ,y)=>{
+    console.log(x *3 );
+});
+
+// Here the array is multiplied with 3 and every array element is multiplied by the function.
+
