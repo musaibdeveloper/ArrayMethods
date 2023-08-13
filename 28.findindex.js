044a7215f03226bdfe522741c8425969b037d9a6
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-13 11:10:13 +0000

    FindIndex is Updated

---
+++ 28.findindex.js
@@ -0,0 +1,8 @@
+// Java script find index method.
+
+// findindex method returns the index of the element which is satisfied the provided function.
+// if the array doesn't satisfies with function then return as -1. 
+
+let numbers = [2,4,6,8]
+
+console.log(numbers.findIndex((x) => x % 2 == 0));
