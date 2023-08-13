b8c6bd8bc82eca44f38e3e41cf74e5078ed4a5e9
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-13 19:42:32 +0000

    Lastof index is Updated

--- 24.Lastofindex.js
+++ 24.Lastofindex.js
@@ -6,7 +6,7 @@
 
 let num = [1,2,6,7,8,9,3]
 
-console.log(num.lastIndexOf(1));
+console.log(num.lastIndexOf(7)); // 3 
 
 // The above last of index takes the input as element which is present in the array, 
 // gives the index value, if the element doesn't exist in the array then it's -1 by default.
