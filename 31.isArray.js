26012be38a4fa3d96192992c825a49c1a7b2c4d6
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-13 17:23:00 +0000

    IsAraay is Updated

---
+++ 31.isArray.js
@@ -0,0 +1,14 @@
+// Is Array.
+
+// isArray(): This method determines whether the
+// passed value is an array or not.
+
+// The output is in boolean Value.
+
+let arr = ["Apple", "Mango", "Grapes"]
+
+console.log(Array.isArray(arr)); // true 
+
+let numbers = 5465465;
+
+console.log(Array.isArray(numbers)); // false.
