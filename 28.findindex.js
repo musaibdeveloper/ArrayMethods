a765384d8c7c0a621178baac6e9d37706350151f
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-13 20:02:03 +0000

    find index is Updated

--- 28.findindex.js
+++ 28.findindex.js
@@ -5,4 +5,12 @@
 
 let numbers = [2,4,6,8]
 
-console.log(numbers.findIndex((x) => x % 2 == 0));
+console.log(numbers.findIndex((x) => x % 2 == 0)); // 0
+
+// The above condition is satisfied so, it returs the 0 
+
+let numbers = [9,13,17,19]
+
+console.log(numbers.findIndex((x) => x % 2 == 0)); // -1
+
+// The elements doesn't pass the test so it returns -1. 
