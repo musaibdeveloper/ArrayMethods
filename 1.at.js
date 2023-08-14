4ee6b65df75e0f22e30701aacc261fc85b3d2f1c
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-14 10:56:12 +0000

    At is Updated

--- 1.at.js
+++ 1.at.js
@@ -1,6 +1,11 @@
 // .Array Methods in Javascript. 
 // It accept the index value and returns the output
 
+// at(): This method returns the element at the
+// specified index in the array. It takes one
+// argument: the index.
+
+
 // If we give any index value which doesn't exist on the array it will show as undefined.
 
 let arr = ["one" , "two", "three"]
