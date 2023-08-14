16dfcf8c534eedebea25bdae76e6ae673f7d44f6
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-14 10:53:25 +0000

    Values are Updated

--- 10.Values.js
+++ 10.Values.js
@@ -1,5 +1,13 @@
 // Array values in array Methods.
 
+
+//values(): This method returns an iterator
+// that provides the values for each index in
+// the array. It takes no arguments.
+ 
+
+
+
 let arr = ["Apple", "banana", "mango", "grapes"]
 
 let result = arr.values
